a5bba3f396268420e49e32ef5039b4d2ce08f6b1
Tyler Walton <tylerwalton300@gmail.com>
2022-12-16 20:17:25 -0500

    added inter and engineer prompts

--- index.js
+++ index.js
@@ -38,7 +38,86 @@ console.log(employeeArray)
 })
 }
 
-managerPrompt ()
+
+function engineerPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the engineers name",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Please enter the engineers github",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the engineers email",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the engineers ID",
+      },
+    ])
+
+    .then((response) => {
+      const instance = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      employeeArray.push(instance);
+      console.log(employeeArray);
+      // add function here to ask if you want a new team member
+    });
+}
+
+
+
+function internPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the interns name",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school did the intern attend?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the interns email",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the interns ID",
+      },
+    ])
+
+    .then((response) => {
+      const instance = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      employeeArray.push(instance);
+      console.log(employeeArray);
+      // add function here to ask if you want a new team member
+    });
+}
+
+
 // const tyler = new Engineer ('tyler', '910', 'tylerwalton300@gmail.com','tylerwalton')
 // console.log(tyler)
 
