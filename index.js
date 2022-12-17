59070f43f15a8121d2711099adec54bf4d5b0520
Tyler Walton <tylerwalton300@gmail.com>
2022-12-16 19:48:56 -0500

    Adding in classes

---
+++ index.js
@@ -0,0 +1,7 @@
+const Engineer = require ('./lib/Engineer')
+const Manager = require ('./lib/Manager')
+const Employee = require ('./lib/Employee')
+const Intern = require ('./lib/Intern') 
+
+const tyler = new Engineer ('tyler', '910', 'tylerwalton300@gmail.com','tylerwalton')
+console.log(tyler)
